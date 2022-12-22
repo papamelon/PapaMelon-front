233bf0088618ea4b97fd1a484a45f757488da726
Junho-06 <k34920032@gmail.com>
2022-12-22 17:20:05 +0900

    기본적인 화면 구현 완료

---
+++ index.js
@@ -0,0 +1,5 @@
+const sex = document.getElementById("invite-button");
+
+sex.addEventListener("click",() => {
+    window.open("https://discord.com/oauth2/authorize?client_id=1054186963474862110&permissions=8&scope=bot")
+})
