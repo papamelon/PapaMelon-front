de22aaa778f5674750c05c6d8e437d3fb443ff49
Junho-06 <k34920032@gmail.com>
2022-12-22 18:18:35 +0900

    button

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const sex = document.getElementById("invite-button");
+const inviteButton = document.getElementById("invite-button");
 
-sex.addEventListener("click",() => {
+inviteButton.addEventListener("click",() => {
     window.open("https://discord.com/oauth2/authorize?client_id=1054186963474862110&permissions=8&scope=bot")
 })
